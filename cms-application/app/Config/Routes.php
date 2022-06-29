18a71ffa1ff8ed8f268c7a23baac8ba0e37d8ec7
Sonam Singh <sonam@thetechmakers.com>
2022-06-29 20:06:32 +0530

    New CI Project created.

--- cms-application/app/Config/Routes.php
+++ cms-application/app/Config/Routes.php
@@ -36,6 +36,12 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/admin/auth', 'Auth::index');
+$routes->get('/login', 'Auth::login');
+$routes->get('/register', 'Auth::register');
+
+$routes->get('/project-add', 'Home::add_project');
+
 
 /*
  * --------------------------------------------------------------------
--- cms-application/app/Controllers/Home.php
+++ cms-application/app/Controllers/Home.php
@@ -5,7 +5,11 @@ namespace App\Controllers;
 class Home extends BaseController
 {
     public function index()
-    {
+    { echo "This is for test."; die;
         return view('welcome_message');
     }
+    public function add_project(){
+        return view('add_project');
+    }
+    
 }
